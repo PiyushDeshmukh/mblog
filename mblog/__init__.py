c3e5d24bc1e7310895a67292db03cf97ea49eb44
Piyush Deshmukh <p.desh31@gmail.com>
2019-09-12 18:12:33 +0530

    adjusting post timestamps and user last seen timestamps according to the client timezone

--- mblog/__init__.py
+++ mblog/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,6 +12,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+
 login.login_view = 'login'
 
 from mblog import routes, models, errors
